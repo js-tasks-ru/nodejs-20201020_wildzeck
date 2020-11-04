6983a233e3a95cb00f18f73d020120de8a969db3
W1ldzeck <wildzeck@gmail.com>
2020-11-05 01:07:52 +0200

    add basic write stream logic

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,23 +1,65 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
+const LimitSizeStream = require('./LimitSizeStream');
+const limitedStream = new LimitSizeStream({ limit: 1000000 });
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+    const pathname = url.parse(req.url).pathname.slice(1);
+    const filepath = path.join(__dirname, 'files', pathname);
+    const writeStream = fs.createWriteStream(filepath, {
+        flags: 'wx',
+    });
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    switch (req.method) {
+        case 'POST':
+            if (isFileInNestedFolder(pathname)) {
+                res.statusCode = 400;
+                res.end('Nested folders are not supported');
+                return;
+            }
 
-  switch (req.method) {
-    case 'POST':
+            req.pipe(limitedStream)
+                .pipe(writeStream)
+                .on('error', (error) => {
+                    if (error.code === 'LIMIT_EXCEEDED') {
+                        res.statusCode = 413;
+                        res.end('File is too big');
+                        return;
+                    }
+                    if (error.code === 'EEXIST') {
+                        res.statusCode = 409;
+                        res.end('Conflict');
+                        return;
+                    }
 
-      break;
+                    if (error.code === 'ENOENT') {
+                        res.statusCode = 404;
+                        res.end('Not found');
+                        return;
+                    }
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+                    res.statusCode = 500;
+                    res.end('Internal server error');
+                })
+                .on('close', () => {
+                    if (res.finished) return;
+                    stream.destroy();
+                })
+
+            break;
+
+        default:
+            res.statusCode = 501;
+            res.end('Not implemented');
+    }
 });
+
+function isFileInNestedFolder(pathname) {
+    return pathname.split('/').length > 1;
+}
 
 module.exports = server;
