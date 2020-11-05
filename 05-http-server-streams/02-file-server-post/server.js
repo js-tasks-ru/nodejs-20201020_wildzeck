80417001a8628100db27cda9cf210db77036dd2f
W1ldzeck <wildzeck@gmail.com>
2020-11-05 13:15:54 +0200

    modified my solution with new error handlers

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -10,9 +10,6 @@ const limitedStream = new LimitSizeStream({ limit: 1000000 });
 server.on('request', (req, res) => {
     const pathname = url.parse(req.url).pathname.slice(1);
     const filepath = path.join(__dirname, 'files', pathname);
-    const writeStream = fs.createWriteStream(filepath, {
-        flags: 'wx',
-    });
 
     switch (req.method) {
         case 'POST':
@@ -22,33 +19,50 @@ server.on('request', (req, res) => {
                 return;
             }
 
+            const writeStream = fs.createWriteStream(filepath, {
+                flags: 'wx',
+            });
+
             req.pipe(limitedStream)
-                .pipe(writeStream)
                 .on('error', (error) => {
                     if (error.code === 'LIMIT_EXCEEDED') {
                         res.statusCode = 413;
                         res.end('File is too big');
                         return;
                     }
+
+                    res.statusCode = 500;
+                    res.end('Internal server error');
+                })
+                .on('close', async () => {
+                    if (res.finished) {
+                        await deleteTheFile(filepath);
+                        stream.destroy();
+                        return;
+                    }
+                })
+                .pipe(writeStream)
+                .on('error', (error) => {
                     if (error.code === 'EEXIST') {
                         res.statusCode = 409;
                         res.end('Conflict');
                         return;
                     }
 
-                    if (error.code === 'ENOENT') {
-                        res.statusCode = 404;
-                        res.end('Not found');
-                        return;
-                    }
-
                     res.statusCode = 500;
                     res.end('Internal server error');
                 })
-                .on('close', () => {
-                    if (res.finished) return;
-                    stream.destroy();
+                .on('close', async () => {
+                    if (res.finished) {
+                        await deleteTheFile(filepath);
+                        stream.destroy();
+                        return;
+                    }
                 })
+                .on('end', () => {
+                    res.statusCode = 201;
+                    res.end('Created');
+                });
 
             break;
 
@@ -60,6 +74,15 @@ server.on('request', (req, res) => {
 
 function isFileInNestedFolder(pathname) {
     return pathname.split('/').length > 1;
+}
+
+function deleteTheFile(filepath) {
+    return new Promise((resolve, reject) => {
+        fs.unlink(filepath, (err) => {
+            if (err) reject(err);
+            resolve();
+        });
+    });
 }
 
 module.exports = server;
