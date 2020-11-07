636dd6e7ad4912c4d2dece3dfcc7ced3b1e320b3
W1ldzeck <wildzeck@gmail.com>
2020-11-07 13:59:54 +0200

    solved 03-file-server-delete task

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,23 +1,47 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
-  const pathname = url.parse(req.url).pathname.slice(1);
+    const pathname = url.parse(req.url).pathname.slice(1);
+    const filepath = path.join(__dirname, 'files', pathname);
 
-  const filepath = path.join(__dirname, 'files', pathname);
+    switch (req.method) {
+        case 'DELETE':
+            if (isFileInNestedFolder(pathname)) {
+                res.statusCode = 400;
+                res.end('Nested folders are not supported');
+                return;
+            }
 
-  switch (req.method) {
-    case 'DELETE':
+            fs.unlink(filepath, (err) => {
+                if (err) {
+                    if (err.code === 'ENOENT') {
+                        res.statusCode = 404;
+                        res.end('Not found');
+                    } else {
+                        res.statusCode = 500;
+                        res.end('Internal server error');
+                    }
+                } else {
+                    res.statusCode = 200;
+                    res.end('OK');
+                }
+            });
 
-      break;
+            break;
 
-    default:
-      res.statusCode = 501;
-      res.end('Not implemented');
-  }
+        default:
+            res.statusCode = 501;
+            res.end('Not implemented');
+    }
 });
+
+function isFileInNestedFolder(pathname) {
+    return pathname.split('/').length > 1;
+}
 
 module.exports = server;
