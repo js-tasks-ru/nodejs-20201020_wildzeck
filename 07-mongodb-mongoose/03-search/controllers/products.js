7682b6e2b42dd3c81e1fa660b6459fedd1f57741
W1ldzeck <wildzeck@gmail.com>
2021-02-01 20:50:32 +0200

    add response refactoring

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -4,15 +4,8 @@ module.exports.productsByQuery = async function productsByQuery(ctx, next) {
     const { query } = ctx.request.query;
     const productsByQuery = await Product.find({ $text: { $search: query } });
 
-    if (!productsByQuery.length) {
-        ctx.response.status = 200;
-        ctx.response.body = {
-            products: [],
-        };
-    }
-
     ctx.response.status = 200;
     ctx.response.body = {
-        products: productsByQuery,
+        products: productsByQuery.length ? productsByQuery : [],
     };
 };
