222f6861af122bec4cea39272265ef3b0da0d702
W1ldzeck <wildzeck@gmail.com>
2021-02-01 17:51:50 +0200

    solved task 03-search

--- 07-mongodb-mongoose/03-search/controllers/products.js
+++ 07-mongodb-mongoose/03-search/controllers/products.js
@@ -1,3 +1,18 @@
+const Product = require('../models/Product');
+
 module.exports.productsByQuery = async function productsByQuery(ctx, next) {
-  ctx.body = {products: []};
+    const { query } = ctx.request.query;
+    const productsByQuery = await Product.find({ $text: { $search: query } });
+
+    if (!productsByQuery.length) {
+        ctx.response.status = 200;
+        ctx.response.body = {
+            products: [],
+        };
+    }
+
+    ctx.response.status = 200;
+    ctx.response.body = {
+        products: productsByQuery,
+    };
 };
