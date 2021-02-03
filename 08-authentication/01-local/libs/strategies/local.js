d1b318eca0009c42f5aade8a0d941a6fc4c0b856
W1ldzeck <wildzeck@gmail.com>
2021-02-03 21:16:59 +0200

    solved task 01-local

--- 08-authentication/01-local/libs/strategies/local.js
+++ 08-authentication/01-local/libs/strategies/local.js
@@ -1,8 +1,21 @@
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/User');
 
 module.exports = new LocalStrategy(
-    {usernameField: 'email', session: false},
-    function(email, password, done) {
-      done(null, false, 'Стратегия подключена, но еще не настроена');
-    },
+    { usernameField: 'email', session: false },
+    async function (email, password, done) {
+        const findUser = await User.findOne({ email: email });
+
+        if (!findUser) {
+            done(null, false, 'Нет такого пользователя');
+        }
+
+        const isValidPassword = await findUser.checkPassword(password);
+
+        if (!isValidPassword) {
+            done(null, false, `Неверный пароль`);
+        }
+
+        done(null, findUser);
+    }
 );
