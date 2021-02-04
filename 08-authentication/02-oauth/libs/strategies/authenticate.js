c97e16164735433c74d787e19e8217114d3d920b
W1ldzeck <wildzeck@gmail.com>
2021-02-04 17:49:33 +0200

    solved task 02-oauth

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,27 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(
+    strategy,
+    email,
+    displayName,
+    done
+) {
+    try {
+        if (!email) {
+            return done(null, false, 'Не указан email');
+        }
+
+        const findUser = await User.findOne({ email: email });
+        if (!findUser) {
+            const newUser = await User.create({
+                email: email,
+                displayName: displayName,
+            });
+            return done(null, newUser);
+        }
+
+        return done(null, findUser);
+    } catch (error) {
+        done(error);
+    }
 };
