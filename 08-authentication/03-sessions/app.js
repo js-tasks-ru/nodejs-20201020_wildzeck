e1b67f355c713ab84a41b2ae099c155e80f24404
W1ldzeck <wildzeck@gmail.com>
2021-02-06 21:39:16 +0200

    solved task 03-sessions

--- 08-authentication/03-sessions/app.js
+++ 08-authentication/03-sessions/app.js
@@ -5,9 +5,9 @@ const Session = require('./models/Session');
 const { v4: uuid } = require('uuid');
 const handleMongooseValidationError = require('./libs/validationErrors');
 const mustBeAuthenticated = require('./libs/mustBeAuthenticated');
-const {login} = require('./controllers/login');
-const {oauth, oauthCallback} = require('./controllers/oauth');
-const {me} = require('./controllers/me');
+const { login } = require('./controllers/login');
+const { oauth, oauthCallback } = require('./controllers/oauth');
+const { me } = require('./controllers/me');
 
 const app = new Koa();
 
@@ -15,37 +15,56 @@ app.use(require('koa-static')(path.join(__dirname, 'public')));
 app.use(require('koa-bodyparser')());
 
 app.use(async (ctx, next) => {
-  try {
-    await next();
-  } catch (err) {
-    if (err.status) {
-      ctx.status = err.status;
-      ctx.body = {error: err.message};
-    } else {
-      console.error(err);
-      ctx.status = 500;
-      ctx.body = {error: 'Internal server error'};
+    try {
+        await next();
+    } catch (err) {
+        if (err.status) {
+            ctx.status = err.status;
+            ctx.body = { error: err.message };
+        } else {
+            console.error(err);
+            ctx.status = 500;
+            ctx.body = { error: 'Internal server error' };
+        }
     }
-  }
 });
 
 app.use((ctx, next) => {
-  ctx.login = async function(user) {
-    const token = uuid();
+    ctx.login = async function (user) {
+        const token = uuid();
+        const newSession = Session.create({
+            token: token,
+            user: user,
+            lastVisit: Date(),
+        });
 
-    return token;
-  };
+        return token;
+    };
 
-  return next();
+    return next();
 });
 
-const router = new Router({prefix: '/api'});
+const router = new Router({ prefix: '/api' });
 
 router.use(async (ctx, next) => {
-  const header = ctx.request.get('Authorization');
-  if (!header) return next();
+    const header = ctx.request.get('Authorization');
+    if (!header) return next();
 
-  return next();
+    const token = ctx.headers.authorization.split(' ')[1];
+    if (!token) {
+        next();
+    }
+    const session = await Session.findOne({ token: 'token' }).populate('user');
+    if (!session) {
+        ctx.throw(401, `Неверный аутентификационный токен`);
+    }
+    await Session.updateOne({ token: token }, { lastVisit: new Date() });
+    ctx.user = {
+        displayName: session.user.displayName,
+        email: session.user.email,
+    };
+
+    return next();
 });
 
 router.post('/login', login);
@@ -53,7 +72,7 @@ router.post('/login', login);
 router.get('/oauth/:provider', oauth);
 router.post('/oauth_callback', handleMongooseValidationError, oauthCallback);
 
-router.get('/me', me);
+router.get('/me', mustBeAuthenticated, me);
 
 app.use(router.routes());
 
@@ -62,10 +81,10 @@ const fs = require('fs');
 
 const index = fs.readFileSync(path.join(__dirname, 'public/index.html'));
 app.use(async (ctx) => {
-  if (ctx.url.startsWith('/api') || ctx.method !== 'GET') return;
+    if (ctx.url.startsWith('/api') || ctx.method !== 'GET') return;
 
-  ctx.set('content-type', 'text/html');
-  ctx.body = index;
+    ctx.set('content-type', 'text/html');
+    ctx.body = index;
 });
 
 module.exports = app;
