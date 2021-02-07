88f1c2da4ae77063886bf4a11a5c649bee8412b8
W1ldzeck <wildzeck@gmail.com>
2021-02-07 15:52:53 +0200

    solved task 01-registration

--- 09-email/01-registration/controllers/login.js
+++ 09-email/01-registration/controllers/login.js
@@ -1,17 +1,25 @@
 const passport = require('../libs/passport');
 
 module.exports.login = async function login(ctx, next) {
-  await passport.authenticate('local', async (err, user, info) => {
-    if (err) throw err;
+    await passport.authenticate('local', async (err, user, info) => {
+        if (err) throw err;
 
-    if (!user) {
-      ctx.status = 400;
-      ctx.body = {error: info};
-      return;
-    }
+        if (user.verificationToken) {
+            ctx.status = 400;
+            ctx.body = {
+                error: 'Подтвердите email',
+            };
+            return;
+        }
 
-    const token = await ctx.login(user);
+        if (!user) {
+            ctx.status = 400;
+            ctx.body = { error: info };
+            return;
+        }
 
-    ctx.body = {token};
-  })(ctx, next);
+        const token = await ctx.login(user);
+
+        ctx.body = { token };
+    })(ctx, next);
 };
--- 09-email/01-registration/controllers/registration.js
+++ 09-email/01-registration/controllers/registration.js
@@ -3,9 +3,47 @@ const User = require('../models/User');
 const sendMail = require('../libs/sendMail');
 
 module.exports.register = async (ctx, next) => {
+    const { email, displayName, password } = ctx.request.body;
+    const token = uuid();
 
+    const newUser = await User.create({
+        email: email,
+        displayName: displayName,
+        password: password,
+        verificationToken: token,
+    });
+
+    await newUser.setPassword(password);
+    await newUser.save();
+
+    const emailResponse = await sendMail({
+        to: email,
+        subject: 'registration verification',
+        locals: { url: `http://localhost:3000/confirm/${token}` },
+        template: 'confirmation',
+    });
+
+    ctx.response.body = {
+        status: 'ok',
+    };
 };
 
 module.exports.confirm = async (ctx, next) => {
-  
+    const { verificationToken } = ctx.request.body;
+    const user = await User.findOne({ verificationToken: verificationToken });
+
+    if (!user) {
+        ctx.response.code = 400;
+        return (ctx.response.body = {
+            error: 'Ссылка подтверждения недействительна или устарела',
+        });
+    }
+
+    user.verificationToken = undefined;
+    await user.save();
+
+    const token = uuid();
+    ctx.response.body = {
+        token: token,
+    };
 };
