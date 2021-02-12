eb36ea7006cf8f2c27cb031466ca7b6860bd501d
W1ldzeck <wildzeck@gmail.com>
2021-02-12 11:05:30 +0200

    solved task 02-order

--- 09-email/02-order/controllers/orders.js
+++ 09-email/02-order/controllers/orders.js
@@ -2,7 +2,32 @@ const Order = require('../models/Order');
 const sendMail = require('../libs/sendMail');
 
 module.exports.checkout = async function checkout(ctx, next) {
+    const { id, email } = ctx.user;
+    const { product, phone, address } = ctx.request.body;
+    const newOrder = await Order.create({
+        user: id,
+        product: product,
+        phone: phone,
+        address: address,
+    });
+
+    await sendMail({
+        to: email,
+        subject: 'order verification',
+        locals: { order: newOrder.id, product: product },
+        template: 'order-confirmation',
+    });
+
+    ctx.response.body = {
+        order: newOrder.id,
+    };
 };
 
 module.exports.getOrdersList = async function ordersList(ctx, next) {
+    const { id } = ctx.user;
+    const userOrders = await Order.find({ user: id });
+
+    ctx.response.body = {
+        orders: userOrders,
+    };
 };
