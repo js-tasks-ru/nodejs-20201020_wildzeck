64359bb63ca365c8933f6424a79b1b1a5c6542db
W1ldzeck <wildzeck@gmail.com>
2021-02-25 13:23:15 +0200

    solved task 10

--- 10-websockets/01-chat/controllers/messages.js
+++ 10-websockets/01-chat/controllers/messages.js
@@ -1,17 +1,17 @@
 const Message = require('../models/Message');
 
 module.exports.messageList = async function messages(ctx, next) {
-    const messages = await Message.find()
+    const messages = await Message.find({chat: ctx.user.id})
         .sort({ date: 1 })
-        .limit(20)
-        .populate('user');
+        .limit(20);
+
 
     ctx.response.body = {
         messages: messages.map((message) => ({
             date: message.date,
             text: message.text,
             id: message.id,
-            user: message.user.displayName,
+            user: message.user
         })),
     };
 };
--- 10-websockets/01-chat/socket.js
+++ 10-websockets/01-chat/socket.js
@@ -35,3 +35,4 @@ function socket(server) {
 }
 
 module.exports = socket;
+
